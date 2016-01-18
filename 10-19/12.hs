401ad377d465ad042f0c7ec86fb4957d4ecfc679
Junpeng Qiu <qjpchmail@gmail.com>
2016-01-18 00:24:15 -0500

    Add 12.hs
    Ans: 76576500

---
+++ 10-19/12.hs
@@ -0,0 +1,24 @@
+import           Data.List
+
+sieve::[Int] -> [Int]
+sieve (hd:rest) =
+  hd:(sieve (filter (\x -> x `mod` hd /= 0) rest))
+
+iter::Int->[Int]->[Int]
+iter n (hd:rest)
+  | n < hd = []
+  | n == hd = [n]
+  | n `mod` hd == 0 = hd:iter (n `div` hd) (hd:rest)
+  | otherwise = iter n rest
+
+getPrimeFactors::Int->[Int]
+getPrimeFactors num =
+  iter num . sieve $ 2:[3,5..]
+
+getNumberOfFactors::Int->Int
+getNumberOfFactors = product . map ((+1) . length) . group . getPrimeFactors
+
+main = do
+  print . head . dropWhile (\x -> getNumberOfFactors x <= 500) $ triangular
+  where
+    triangular = scanl (+) 1 [2..]
